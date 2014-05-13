b1ff6ea3f16af243096c972f65af296153b60649
Aleksandr Ponomarenko <mcmegavolt@gmail.com>
2014-05-13 14:02:09 +0300

    mailer config

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,5 +1,5 @@
 class NotificationMailer < ActionMailer::Base
-  default from: "no-reply@evestudio.com.ua"
+  default from: "no-reply@xn--b1ayh2f.xn--j1amh"
 
   def signed_up(petition)
     @petition = petition
