6567e17b3f630d6321681f6b383fc03b73d6a598
Aleksandr Ponomarenko <mcmegavolt@gmail.com>
2014-06-16 11:19:53 +0300

    try to fix dublicates

--- app/models/petition.rb
+++ app/models/petition.rb
@@ -3,7 +3,7 @@ class Petition < ActiveRecord::Base
 	after_create :notify_user
 
 	validates_presence_of :first_name, :last_name, :email
-	validates_uniqueness_of :email
+	validates_uniqueness_of :email, :on => :create, :message => 'Один email не може бути використаний більше одного разу.'
 
 	def notify_user
 		# NotificationMailer.delay.signed_up(self)
